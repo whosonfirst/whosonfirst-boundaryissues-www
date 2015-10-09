bdd4d48366e15e9c5dfb0bcf7fbc2ecd933f83b8
thisisaaronland <aaron@mapzen.com>
2015-10-09 18:53:38 +0000

    make the postgres setup script actually work

--- ubuntu/setup-postgres.sh
+++ ubuntu/setup-postgres.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-sudo su -m postgres
-createdb whosonfirst
-psql -d whosonfirst -c "CREATE EXTENSION postgis;"
-psql -d whosonfirst -c "CREATE EXTENSION postgis_topology;"
-psql -d whosonfirst -c "CREATE TABLE whosonfirst (id BIGINT PRIMARY KEY, parent_id BIGINT, placetype VARCHAR, properties TEXT, geom GEOGRAPHY(MULTIPOLYGON, 4326), centroid GEOGRAPHY(POINT, 4326));"
-psql -d whosonfirst -c "CREATE INDEX by_geom ON whosonfirst USING GIST(geom);"
-psql -d whosonfirst -c "CREATE INDEX by_placetype ON whosonfirst (placetype);"
-psql -d whosonfirst -c "VACUUM ANALYZE;"
+sudo -u postgres createdb whosonfirst
+sudo -u postgres psql -d whosonfirst -c "CREATE EXTENSION postgis;"
+sudo -u postgres psql -d whosonfirst -c "CREATE EXTENSION postgis_topology;"
+sudo -u postgres psql -d whosonfirst -c "CREATE TABLE whosonfirst (id BIGINT PRIMARY KEY, parent_id BIGINT, placetype VARCHAR, properties TEXT, geom GEOGRAPHY(MULTIPOLYGON, 4326), centroid GEOGRAPHY(POINT, 4326));"
+sudo -u postgres psql -d whosonfirst -c "CREATE INDEX by_geom ON whosonfirst USING GIST(geom);"
+sudo -u postgres psql -d whosonfirst -c "CREATE INDEX by_placetype ON whosonfirst (placetype);"
+sudo -u postgres psql -d whosonfirst -c "VACUUM ANALYZE;"
