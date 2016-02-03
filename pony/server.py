0c785480c96f9556c295ad721f92216b1a977971
Dan Phiffer <dan@phiffer.org>
2016-02-03 14:35:39 -0500

    🦄 GeoJSON-encoding pony

---
+++ pony/server.py
@@ -0,0 +1,22 @@
+import StringIO
+import sys
+from flask import Flask, request
+
+import geojson
+import mapzen.whosonfirst.geojson
+
+app = Flask(__name__)
+
+@app.route('/geojson-encode', methods=['POST'])
+def geojson_encode():
+    g = request.form['geojson']
+    f = geojson.loads(g)
+    e = mapzen.whosonfirst.geojson.encoder(precision=None)
+    fh = StringIO.StringIO()
+    e.encode_feature(f, fh)
+    fh.seek(0)
+    return fh.read()
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
