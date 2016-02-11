4b520d3185cbdeeda5a096ff2f7f8e68ec9c9ee8
Dan Phiffer <dan@phiffer.org>
2016-02-11 18:57:13 -0500

    🐛 In progress JSON schema fields

--- www/include/lib_wof_venue.php
+++ www/include/lib_wof_venue.php
@@ -1,7 +1,12 @@
 <?php
 	loadlib('wof_utils');
 	loadlib('artisanal_integers');
+	loadlib('json_schema_fields');
 
 	function wof_venue_create() {
-		
+
+	}
+
+	function wof_venue_fields() {
+		return json_schema_fields('whosonfirst.schema');
 	}
--- www/venue.php
+++ www/venue.php
@@ -1,5 +1,6 @@
 <?php
 	include('include/init.php');
+	loadlib('wof_venue');
 
 	login_ensure_loggedin();
 
@@ -16,54 +17,7 @@
 	$crumb_venue = crumb_generate('api', 'wof.venue');
 	$GLOBALS['smarty']->assign("crumb_venue", $crumb_venue);
 
-	$GLOBALS['smarty']->assign('fields', array(
-		//'wof:id' => 'number',
-		'wof:parent_id' => array(
-			'type' => 'number', 'default' => '-1'
-		),
-		'wof:name' => array(
-			'type' => 'text'
-		),
-		'wof:placetype' => array(
-			'type' => 'text'
-		),
-		'wof:country' => array(
-			'type' => 'text'
-		),
-		'wof:concordances' => array(
-			'type' => 'dictionary'
-		),
-		'wof:hierarchy' => array(
-			'type' => 'list'
-		),
-		'wof:belongsto' => array(
-			'type' => 'list'
-		),
-		'wof:supersedes' => array(
-			'type' => 'list'
-		),
-		'wof:superseded_by' => array(
-			'type' => 'list'
-		),
-		'wof:breaches' => array(
-			'type' => 'list'
-		),
-		'wof:tags' => array(
-			'type' => 'list'
-		),
-		'iso:country' => array(
-			'type' => 'text'
-		),
-		'src:geom' => array(
-			'type' => 'text'
-		),
-		'edtf:inception' => array(
-			'type' => 'text', 'default' => 'uuuu'
-		),
-		'edtf:cessation' => array(
-			'type' => 'text', 'default' => 'uuuu'
-		)
-	));
+	$GLOBALS['smarty']->assign('fields', wof_venue_fields());
 
 	$GLOBALS['smarty']->display('page_venue.txt');
 	exit();
