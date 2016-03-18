6daef663c2c54dedd04d2ec839c75cf5d534ec04
thisisaaronland <aaron@mapzen.com>
2016-03-18 22:22:43 +0000

    add mapzen target to Makefile; update mapzen things; add global search box; add try/catch block to wof-geojson-server

--- services/geojson-server/wof-geojson-server.py
+++ services/geojson-server/wof-geojson-server.py
@@ -20,42 +20,78 @@ app = Flask(__name__)
 
 @app.route('/encode', methods=['POST'])
 def geojson_encode():
-    g = request.form['geojson']
-    f = geojson.loads(g)
+
+    try:
+        g = request.form['geojson']
+        f = geojson.loads(g)
+    except Exception, e:
+        error = "failed to load geojson, because %s" % e
+        logging.error(error)
+        return flask.jsonify(ok=0, error=error)
+
     e = mapzen.whosonfirst.geojson.encoder(precision=None)
-    fh = StringIO.StringIO()
-    e.encode_feature(f, fh)
+
+    try:
+        fh = StringIO.StringIO()
+        e.encode_feature(f, fh)
+    except Exception, e:
+        error = "failed to encode geojson, because %s" % e
+        logging.error(error)
+
+        return flask.jsonify(ok=0, error=error)
+
     fh.seek(0)
     encoded = fh.read()
+
     return jsonify(ok=1, encoded=encoded)
+
 @app.route('/save', methods=['POST'])
 def geojson_save():
-    g = request.form['geojson']
-    f = geojson.loads(g)
+
+    try:
+        g = request.form['geojson']
+        f = geojson.loads(g)
+    except Exception, e:
+        err = "failed to load geojson, because %s" % e
+        return flask.jsonify(ok=0, error=err)
 
     # Does the input pass the smell check?
     validation = geojson.is_valid(f)
+
     if (validation['valid'] == 'no'):
         error = "GeoJSON doesn't smell right: %s" % validation['message']
         logging.error(error)
         return jsonify(ok=0, error=error)
 
-    ff = mapzen.whosonfirst.export.flatfile(root, debug=False)
-    path = ff.export_feature(f)
+    try:
+        ff = mapzen.whosonfirst.export.flatfile(root, debug=False)
+        path = ff.export_feature(f)
+    except Exception, e:
+        error = "failed to export geojson, because %s" % e
+        logging.error(error)
+
+        return flask.jsonify(ok=0, error=error)
 
     # Repeat back the file we just wrote
-    gf = open(path)
+    gf = open(path, 'r')
     return gf.read()
+
 @app.route('/update_elasticsearch', methods=['POST'])
 def geojson_update_elasticsearch():
     id = int(request.form['id'])
     path = mapzen.whosonfirst.utils.id2abspath(root, id)
     idx = mapzen.whosonfirst.search.index()
+
     try:
         idx.index_file(path)
     except Exception, e:
-        logging.error("failed to index %s, because %s" % (path, e))
+        error = "failed to index %s, because %s" % (path, e)
+        logging.error(error)
+        
+        return jsonify(ok=0, error=error)
+
     return jsonify(ok=1)
+
 @app.route('/pip', methods=['GET'])
 def geojson_hierarchy():
     data_endpoint = 'https://whosonfirst.mapzen.com/data/'
@@ -66,8 +102,22 @@ def geojson_hierarchy():
     if (mapzen.whosonfirst.placetypes.is_valid_placetype(placetype) == False):
         return jsonify(ok=0, error="What is that placetype?")
 
-    parents = mapzen.whosonfirst.pip.utils.get_reverse_geocoded(lat, lng, placetype)
-    hierarchy = mapzen.whosonfirst.pip.utils.get_hierarchy(parents, data_endpoint)
+    try:
+        parents = mapzen.whosonfirst.pip.utils.get_reverse_geocoded(lat, lng, placetype)
+    except Exception, e:
+        error = "failed to determine parents, because %s" % e
+        logging.error(error)
+
+        return jsonify(ok=0, error=error)
+
+    try:
+        hierarchy = mapzen.whosonfirst.pip.utils.get_hierarchy(parents, data_endpoint)
+    except Exception, e:
+        error = "failed to determine hierarchy, because %s" % e
+        logging.error(error)
+
+        return jsonify(ok=0, error=error)
+
     return jsonify(ok=1, hierarchy=hierarchy, parents=parents)
 
 if __name__ == "__main__":
