8e17e673b359cba4e6f34acb8ff0cefc80cede3a
thisisaaronland <aaron@mapzen.com>
2016-03-19 02:57:01 +0000

    indenting; caveats about pagination

--- www/search.php
+++ www/search.php
@@ -1,69 +1,87 @@
 <?php
 
-include('include/init.php');
+	include('include/init.php');
+	loadlib("elasticsearch");
 
-$query = get_str('q');
+	$args = array();
 
-if ($query) {
-	$GLOBALS['smarty']->assign('query', $query);
-	$url = "{$GLOBALS['cfg']['es_base_url']}_search?size=25";
-	$query = json_encode(array(
-		'query' => array(
-			// Search result relevance is determined by a combining multiple matches.
-			// The overall score is based on combining each of the matches.
-			'bool' => array(
-				'should' => array(
-					// #1: wof:name ('name_not_analyzed' field) is an exact match.
-					array(
-						'match' => array(
-							'name_not_analyzed' => $query
-						)
-					),
-					// #2: wof:name property has all the words.
-					array(
-						'match' => array(
-							'wof:name' => array(
-								'operator' => 'and',
-								'query' => $query
+	$query = get_str('q');
+	$page = get_int32('page');
+
+	if ($page){
+		$args['page'] = $page;
+	}
+
+	if ($query){
+
+		$GLOBALS['smarty']->assign('query', $query);
+
+		$url = "{$GLOBALS['cfg']['es_base_url']}_search?size=25";
+
+		$query = json_encode(array(
+			'query' => array(
+				// Search result relevance is determined by a combining multiple matches.
+				// The overall score is based on combining each of the matches.
+				'bool' => array(
+					'should' => array(
+						// #1: wof:name ('name_not_analyzed' field) is an exact match.
+						array(
+							'match' => array(
+								'name_not_analyzed' => $query
+							)
+						),
+						// #2: wof:name property has all the words.
+						array(
+							'match' => array(
+								'wof:name' => array(
+									'operator' => 'and',
+									'query' => $query
+								)
+							)
+						),
+						// #3: any of the name:* properties (appended to 'name_all') have
+						//     any of the words.
+						array(
+							'match' => array(
+								'name_all' => $query
 							)
 						)
 					),
-					// #3: any of the name:* properties (appended to 'name_all') have
-					//     any of the words.
-					array(
+					'must' => array(
+						// #4: any of the words appear anywhere in the WOF record.
 						'match' => array(
-							'name_all' => $query
+							'_all' => $query
 						)
-					)
-				),
-				'must' => array(
-					// #4: any of the words appear anywhere in the WOF record.
-					'match' => array(
-						'_all' => $query
 					)
 				)
 			)
-		)
-	));
-	$rsp = http_post($url, $query);
-	if ($rsp['ok']) {
-		$results = array();
-		$body = json_decode($rsp['body'], true);
-		foreach ($body['hits']['hits'] as $hit) {
+		));
 
-			$results[] = array(
-				'id' => $hit['_source']['wof:id'],
-				'name' => $hit['_source']['wof:name'],
-				'placetype' => $hit['_source']['wof:placetype'],
-				'lat' => $hit['_source']['geom:latitude'],
-				'lng' => $hit['_source']['geom:longitude'],
-				'is_current' => $hit['_source']['mz:is_current'],
-				'deprecated' => $hit['_source']['edtf:deprecated'],
-			);
+		$rsp = http_post($url, $query);
+
+		if ($rsp['ok']){
+
+			$results = array();
+			$body = json_decode($rsp['body'], true);
+
+			foreach ($body['hits']['hits'] as $hit) {
+
+				$results[] = array(
+					'id' => $hit['_source']['wof:id'],
+					'name' => $hit['_source']['wof:name'],
+					'placetype' => $hit['_source']['wof:placetype'],
+					'lat' => $hit['_source']['geom:latitude'],
+					'lng' => $hit['_source']['geom:longitude'],
+					'is_current' => $hit['_source']['mz:is_current'],
+					'deprecated' => $hit['_source']['edtf:deprecated'],
+				);
+			}
+
+			$GLOBALS['smarty']->assign('results', $results);
 		}
-		$GLOBALS['smarty']->assign('results', $results);
 	}
-}
 
-$GLOBALS['smarty']->display('page_search.txt');
-exit();
+	$GLOBALS['smarty']->display('page_search.txt');
+	exit();
+
+?>
