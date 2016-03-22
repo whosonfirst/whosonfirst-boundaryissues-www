32e5dcd0cfe1a3be1b52b9766a76bc0fff24163e
thisisaaronland <aaron@mapzen.com>
2016-03-22 14:37:58 +0000

    first pas at tags

---
+++ www/tag.php
@@ -0,0 +1,54 @@
+<?php
+
+	include("include/init.php");
+	loadlib("elasticsearch");
+
+	$args = array(
+		'index' => 'whosonfirst'
+	);
+
+	$page = get_int32('page');
+
+	if ($page){
+		$args['page'] = $page;
+	}
+
+	$tag = get_str("tag");
+	$tag = filter_strict($tag);
+
+	if (! $tag){
+		error_404();
+	}
+
+	$es_query = array(
+		'query' => array('filtered' => array(
+			'query' => array(
+				'match_all' => array()
+			),
+			'filter' => array(
+				'term' => array('wof:tags' => $tag)
+			)
+		)),
+		'sort' => array(
+			array('wof:lastmodified' => 'desc'),
+			array('wof:id' => 'desc'),		# absent wof:created...
+		)
+	);
+
+	$rsp = elasticsearch_search($es_query, $args);
+
+	$pagination = $rsp['pagination'];
+	$results = $rsp['rows'];
+
+	$GLOBALS['smarty']->assign_by_ref("pagination", $pagination);
+	$GLOBALS['smarty']->assign_by_ref("results", $results);
+
+	$enc_tag = urlencode($tag);
+
+	$pagination_url = $GLOBALS['cfg']['abs_root_url'] . "tags/{$enc_tag}/";
+	$GLOBALS['smarty']->assign("pagination_url", $pagination_url);
+
+	$GLOBALS['smarty']->assign_by_ref("tag", $tag);
+	
+	$GLOBALS['smarty']->display('page_tag.txt');
+	exit();
