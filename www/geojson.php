fd0a4239e8a40f49153478c02dcc1ab1c50eb9c4
Dan Phiffer <dan@phiffer.org>
2016-03-23 14:32:37 -0400

    ▶️ Indenting Re: #70

--- www/geojson.php
+++ www/geojson.php
@@ -1,22 +1,22 @@
 <?php
 
-include('include/init.php');
-loadlib('wof_utils');
+	include('include/init.php');
+	loadlib('wof_utils');
 
-$id = get_int64('id');
-$download = get_int64('download');
-$path = wof_utils_id2abspath($GLOBALS['cfg']['wof_data_dir'], $id);
+	$id = get_int64('id');
+	$download = get_int64('download');
+	$path = wof_utils_id2abspath($GLOBALS['cfg']['wof_data_dir'], $id);
 
-if (! $id || ! file_exists($path)) {
-	error_404();
-}
+	if (! $id || ! file_exists($path)) {
+		error_404();
+	}
 
-if ($download) {
-	$filename = basename($path);
-	header("Content-Type: application/octet-stream");
-	header("Content-Transfer-Encoding: Binary");
-	header("Content-disposition: attachment; filename=\"$filename\"");
-} else {
-	header('Content-Type: application/json');
-}
-echo file_get_contents($path);
+	if ($download) {
+		$filename = basename($path);
+		header("Content-Type: application/octet-stream");
+		header("Content-Transfer-Encoding: Binary");
+		header("Content-disposition: attachment; filename=\"$filename\"");
+	} else {
+		header('Content-Type: application/json');
+	}
+	echo file_get_contents($path);
