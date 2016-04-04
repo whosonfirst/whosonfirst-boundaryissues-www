bd3a18c049c281584c69e60267ce17d878661487
thisisaaronland <aaron@mapzen.com>
2016-04-04 23:20:42 +0000

    pull in dphiffer's py-mz-wof-search

--- ubuntu/setup-py-mapzen.sh
+++ ubuntu/setup-py-mapzen.sh
@@ -17,5 +17,25 @@ else
     cd /usr/local/mapzen/py-mapzen-whosonfirst
 fi 
 
-sudo python setup.py install
+cd -
+
+# THIS IS A HACK WHILE WE WAIT TO MOVE / TOGGLE THE SPELUNKER FROM THE SANDBOX
+# TO PROD/DEV - YOU MAY BE ASKING YOURSELF WHY THE SPELUNKER IS REACHING ITS
+# BRUBBY LITTLE HANDS IN HERE AND THE ANSWER IS... YEAH, PLEASE REMOVE THIS AS
+# SOON AS POSSIBLE (20160404/thisisaaronland)
+
+if [ -d /usr/local/mapzen/py-mapzen-whosonfirst-search ]
+then
+    cd /usr/local/mapzen/py-mapzen-whosonfirst-search
+else
+    git clone git@github.com:whosonfirst/py-mapzen-whosonfirst-search.git /usr/local/mapzen/py-mapzen-whosonfirst-search
+    cd /usr/local/mapzen/py-mapzen-whosonfirst
+    git fetch
+fi 
+
+git checkout dphiffer/boundary-issues
+git pull dphiffer/boundary-issues master
+
+sudo python ./setup.py install
+
 cd -
