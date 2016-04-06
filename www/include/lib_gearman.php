20e9fd47c002da9161a5ea9a59cd702bdaa2f381
Dan Phiffer <dan@phiffer.org>
2016-04-06 11:53:00 -0400

    New library: lib_gearman

---
+++ www/include/lib_gearman.php
@@ -0,0 +1,79 @@
+<?php
+
+	########################################################################
+
+	function gearman_get_client() {
+		if (! $GLOBALS['gearman_client']) {
+			$gearman_client = new GearmanClient();
+			$server_added = $gearman_client->addServer(
+				$GLOBALS['cfg']['gearman_host'],
+				$GLOBALS['cfg']['gearman_port']
+			);
+			if (! $server_added) {
+				// Couldn't connect to the server
+				return null;
+			}
+			$GLOBALS['gearman_client'] = $gearman_client;
+		}
+		return $GLOBALS['gearman_client'];
+	}
+
+	########################################################################
+
+	function gearman_background_job($job_name, $args = null) {
+
+		$gearman_client = gearman_get_client();
+		if (! $gearman_client) {
+			return array(
+				'ok' => 0,
+				'error' => "Couldn't connect to the Gearman server."
+			);
+		}
+
+		$args = serialize($args);
+		$job_id = gearman_generate_job_id($job_name, $args);
+		$handle = $gearman_client->doBackground($job_name, $args, $job_id);
+
+		if ($gearman_client->returnCode() != GEARMAN_SUCCESS) {
+			$code = $gearman_client->returnCode();
+			$description = gearman_error_description($code);
+			return array(
+				'ok' => 0,
+				'error' => "[$code] $description"
+			);
+		}
+
+		return array(
+			'ok' => 1,
+			'job_id' => $job_id,
+			'handle' => $handle
+		);
+	}
+
+	########################################################################
+
+	function gearman_generate_job_id($job_name, $serialized_args) {
+
+		$user_id = 0;
+		if ($GLOBALS['cfg']['user']['id']) {
+			$user_id = $GLOBALS['cfg']['user']['id'];
+		}
+
+		$timestamp = time();
+		$hash = md5("{$job_name}{$user_id}{$timestamp}{$serialized_args}");
+		$job_id = "{$job_name}_{$user_id}_{$timestamp}_{$hash}";
+		$job_id = substr($job_id, 0, 64); // GEARMAN_MAX_UNIQUE_SIZE = 64
+		return $job_id;
+	}
+
+	########################################################################
+
+	function gearman_error_description($code) {
+		// TODO: add more detailed error messages
+		// http://php.net/manual/en/gearmanclient.error.php
+		// http://php.net/manual/en/gearmanclient.geterrno.php
+		// http://php.net/manual/en/gearmanclient.returncode.php
+		return "Gearman failed: $code";
+	}
+
+	########################################################################
