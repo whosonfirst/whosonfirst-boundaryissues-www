d385dc4c2a628fad464bb5304de057e5d91654a1
thisisaaronland <aaron@mapzen.com>
2016-04-19 20:43:03 +0000

    ensure redis-tools when setting up the server

--- ubuntu/setup-redis-server.sh
+++ ubuntu/setup-redis-server.sh
@@ -11,4 +11,17 @@ git pull origin master
 ./ubuntu/setup-redis-server.sh
 
 cd -
+
+if [ ! -d /usr/local/mapzen/redis-tools ]
+then
+    git clone https://github.com/whosonfirst/redis-tools.git /usr/local/mapzen/redis-tools
+fi 
+
+cd /usr/local/mapzen/redis-tools
+git pull origin master
+
+cd -
+
+sudo easy_install redis
+
 exit 1
