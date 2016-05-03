c87b45f2f413f252736cf4d845c7532a052e4148
Dan Phiffer <dan@phiffer.org>
2016-05-03 17:57:45 -0400

    Set up schema mappings if they haven't been already

--- ubuntu/setup-elasticsearch-index.sh
+++ ubuntu/setup-elasticsearch-index.sh
@@ -6,6 +6,25 @@ PARENT=`dirname $WHOAMI`
 PROJECT=`dirname $PARENT`
 DATA=$1
 
+if [ ! -d /usr/local/mapzen/es-whosonfirst-schema ]
+then
+    git clone https://github.com/whosonfirst/es-whosonfirst-schema.git /usr/local/mapzen/es-whosonfirst-schema
+fi
+
+cd /usr/local/mapzen/es-whosonfirst-schema
+git pull origin master
+cd -
+
+# If the schemas haven't been set already, run them for the first time
+
+if [ ! -f /usr/local/mapzen/es-whosonfirst-schema/BOUNDARYISSUES_INDEX_VERSION ] ; then
+	/usr/local/mapzen/es-whosonfirst-schema/bin/update-schema.sh boundaryissues
+fi
+
+if [ ! -f /usr/local/mapzen/es-whosonfirst-schema/OFFLINE_TASKS_INDEX_VERSION ] ; then
+	/usr/local/mapzen/es-whosonfirst-schema/bin/update-schema.sh offline_tasks
+fi
+
 if [ ! -d ${DATA} ]
 then
     echo "Can not find data directory ${DATA}"
