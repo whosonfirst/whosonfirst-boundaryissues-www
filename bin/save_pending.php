49ff37b8b62c6918871e2964845d4eeea4fcf90f
Dan Phiffer <dan@phiffer.org>
2016-05-06 17:18:51 -0400

    Log save_pending to logstash

--- bin/save_pending.php
+++ bin/save_pending.php
@@ -1,10 +1,35 @@
 <?php
 
 	include("init_local.php");
-	loadlib('wof_save');
+	loadlib("wof_save");
+	loadlib("offline_tasks");
+	loadlib("uuid");
+	loadlib("logstash");
+
+	$task_id = uuid_v4();
+	$now = offline_tasks_microtime();
+	$event = array(
+		'action' => 'schedule',
+		'task_id' => $task_id,
+		'task' => 'save_pending',
+		'data' => array(),
+		'rsp' => array(),
+		'microtime' => $now
+	);
+	logstash_publish('offline_tasks', $event);
 
 	$rsp = wof_save_pending();
-	print_r($rsp);
+
+	$now = offline_tasks_microtime();
+	$event = array(
+		'action' => 'execute',
+		'task_id' => $task_id,
+		'task' => 'save_pending',
+		'data' => array(),
+		'rsp' => $rsp,
+		'microtime' => $now
+	);
+	logstash_publish('offline_tasks', $event);
 
 	exit();
 ?>
