3d9087569bb5e7d70aca513f90131441d1ef4d58
Dan Phiffer <dan@phiffer.org>
2016-07-15 11:46:21 -0400

    :chart_with_upwards_trend: Download and index All of the Places™

---
+++ bin/index_data.php
@@ -0,0 +1,88 @@
+<?php
+
+	include(__DIR__ . "/init_local.php");
+
+	// This script generates a very simple MySQL index of all the WOF
+	// records in a given repo (or subdirectory). I'm not sure whether
+	// this is something we should actually do in Boundary Issues proper,
+	// so consider it exploratory at this point. (20160715/dphiffer)
+
+	// Usage: php index_data.php /path/to/repo [repo name] [comma-separated IDs to skip (i.e., NZ)]
+
+	if (substr($argv[0], -14, 14) == 'index_data.php' &&
+	    ! empty($argv[1])) {
+		$dir = $argv[1];
+		if (substr($dir, 0, 1) != '/') {
+			$dir = __DIR__ . "/$dir";
+		}
+		if (! file_exists($dir)) {
+			die("Directory $dir doesn't exist!\n");
+		}
+		if (file_exists("$dir/data")) {
+			$repo = basename($dir);
+			$dir = "$dir/data";
+		}
+		if (! empty($argv[2])) {
+			$repo = $argv[2];
+		} else if (empty($repo)) {
+			die("Please pass a second repo argument.\n");
+		}
+		if (! empty($argv[3])) {
+			$skip = explode(',', $argv[3]);
+		}
+		index_dir($dir, $repo, $skip);
+	}
+
+	function index_dir($dir, $repo, $skip = null) {
+		$dh = opendir($dir);
+		$subdirs = array();
+		while (($file = readdir($dh)) !== false) {
+			if (preg_match('/^\d+$/', $file)) {
+				$subdirs[] = $file;
+			} else if (preg_match('/^(\d+)\.geojson$/', $file, $matches)) {
+				if (! empty($skip) &&
+				    in_array($matches[1], $skip)) {
+					continue;
+				}
+				index_file("$dir/$file", $repo);
+			}
+		}
+		closedir($dh);
+		sort($subdirs, SORT_NUMERIC);
+		foreach ($subdirs as $subdir) {
+			index_dir("$dir/$subdir", $repo, $skip);
+		}
+	}
+
+	function index_file($path, $repo) {
+		echo "Indexing $path\n";
+		$json = file_get_contents($path);
+		$feature = json_decode($json, 'as hash');
+		$props = $feature['properties'];
+		$hash = array(
+			'id' => addslashes($props['wof:id']),
+			'name' => addslashes($props['wof:name']),
+			'repo' => addslashes($repo)
+		);
+		if ($props['lbl:latitude']) {
+			$hash['latitude'] = addslashes($props['lbl:latitude']);
+		} else if ($props['geom:latitude']) {
+			$hash['latitude'] = addslashes($props['geom:latitude']);
+		}
+		if ($props['lbl:longitude']) {
+			$hash['longitude'] = addslashes($props['lbl:longitude']);
+		} else if ($props['geom:longitude']) {
+			$hash['longitude'] = addslashes($props['geom:longitude']);
+		}
+		if ($props['geom:bbox']) {
+			$bbox = explode(',', $props['geom:bbox']);
+			$hash['bbox0'] = addslashes($bbox[0]);
+			$hash['bbox1'] = addslashes($bbox[1]);
+			$hash['bbox2'] = addslashes($bbox[2]);
+			$hash['bbox3'] = addslashes($bbox[3]);
+		}
+		$where = array(
+			'id' => addslashes($props['wof:id'])
+		);
+		db_insert_dupe('boundaryissues_wof', $hash, $where);
+	}
---
+++ bin/update_data.php
@@ -0,0 +1,63 @@
+<?php
+
+	include(__DIR__ . "/init_local.php");
+	loadlib('github_users');
+	loadlib('github_api');
+	loadlib('git');
+
+	// This script clones (or pulls in updates from) all the repos in the
+	// whosonfirst-data org. It skips things that don't conform to the
+	// pattern whosonfirst-data-venue-* and also skips duplicates like
+	// whosonfirst-data-venue-us and whosonfirst-data-venue, where records
+	// are available in more granular repos. (20160715/dphiffer)
+
+	// We're not doing anything privileged with the token, so just grab one.
+	$rsp = db_single("
+		SELECT oauth_token
+		FROM GithubUsers
+		ORDER BY RAND()
+		LIMIT 1
+	");
+	$oauth_token = $rsp['oauth_token'];
+
+	$page = 1;
+	$count = 0;
+	$rsp = github_api_call('GET', "orgs/whosonfirst-data/repos?page=$page", $oauth_token);
+	$repos = [];
+
+	echo "Loading whosonfirst-data repos";
+
+	while (! empty($rsp['rsp'])) {
+		foreach ($rsp['rsp'] as $repo) {
+			if (preg_match('/whosonfirst-data-venue-/', $repo['name'])) {
+				if ($repo['name'] == 'whosonfirst-data-venue-us') {
+					// Skip this since it's split into individual state repos
+					continue;
+				}
+				//echo "{$repo['name']}\n";
+				$repos[] = $repo['name'];
+				$count++;
+			}
+		}
+		echo ".";
+		$page++;
+		$rsp = github_api_call('GET', "orgs/whosonfirst-data/repos?page=$page", $oauth_token);
+	}
+
+	echo "\n";
+
+	sort($repos);
+	//echo "Total repos: $count\n";
+
+	foreach ($repos as $repo) {
+		$repo_dir = "/usr/local/data/$repo";
+		if (! file_exists($repo_dir)) {
+			$repo_url = "https://github.com/whosonfirst-data/$repo.git";
+			echo "git clone $repo_url\n";
+			git_clone('/usr/local/data', $repo_url);
+		} else {
+			echo "git pull $repo_dir\n";
+			git_pull($repo_dir);
+		}
+		//index_dir("$repo_dir/data", $repo);
+	}
