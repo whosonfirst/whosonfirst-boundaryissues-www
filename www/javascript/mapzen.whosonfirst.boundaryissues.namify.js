174d5c385764858a58e0955da9f4f289c8aab854
Dan Phiffer <dan@phiffer.org>
2016-08-30 12:13:30 -0700

    :wrench: Use the local resolver first, then fall back on the cloud

--- www/javascript/mapzen.whosonfirst.boundaryissues.namify.js
+++ www/javascript/mapzen.whosonfirst.boundaryissues.namify.js
@@ -22,8 +22,8 @@ mapzen.whosonfirst.boundaryissues.namify = (function() {
 
 			for (var i=0; i < count; i++){
 				self.namify_el(els[i], [
-					mapzen.whosonfirst.data.id2abspath,
-					self.local_resolver
+					self.local_resolver,
+					mapzen.whosonfirst.data.id2abspath
 				]);
 			}
 		},
