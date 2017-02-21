9cbc5c5d6bda599ab27b142045b897e046c4ef9c
Dan Phiffer <dan@phiffer.org>
2017-02-21 11:57:31 -0500

    :floppy_disk: basic venue saving

--- www/venue.php
+++ www/venue.php
@@ -1,12 +1,15 @@
 <?php
 
 	include('include/init.php');
-	
+
 	$id = get_int64('id');
 	if ($id) {
 		$GLOBALS['smarty']->assign('page_title', 'Edit venue');
 	} else {
 		$GLOBALS['smarty']->assign('page_title', 'Add venue');
 	}
-	
+
+	$crumb_save = crumb_generate('api', 'wof.save');
+	$GLOBALS['smarty']->assign('crumb_save', $crumb_save);
+
 	$GLOBALS['smarty']->display('page_venue.txt');
