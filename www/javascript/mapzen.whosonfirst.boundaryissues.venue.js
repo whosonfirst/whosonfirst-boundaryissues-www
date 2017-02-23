3fbc22b2c11e1063ab86e7541b6f24bbf50ac7e4
Dan Phiffer <dan@phiffer.org>
2017-02-23 12:44:46 -0500

    :package: add default bbox link to venue page

--- www/javascript/mapzen.whosonfirst.boundaryissues.venue.js
+++ www/javascript/mapzen.whosonfirst.boundaryissues.venue.js
@@ -12,48 +12,28 @@ mapzen.whosonfirst.boundaryissues.venue = (function() {
 
 	var self = {
 		map: null,
-		country_id: -1,
 		properties: {},
+		country_id: -1,
 
-		set_country: function(country_id, cb) {
+		set_property: function(name, value) {
+			self.properties[name] = value;
+		},
 
-			if (country_id == self.country_id) {
+		set_country: function(country) {
+
+			if (country.properties['wof:id'] == self.country_id) {
 				// No change in the ID, so no need to reload the WOF record
 				return;
 			}
 
-			var base_url = $('body').data('data-abs-root-url');
-			var relpath = mapzen.whosonfirst.data.id2relpath(country_id);
-			var url = base_url + relpath;
+			var props = country.properties;
 
-			var on_success = function(rsp) {
-				if (! rsp.properties) {
-					mapzen.whosonfirst.log.error('Tried to set country properties, but the country WOF record had no properties.');
-					return;
-				}
-				var props = rsp.properties;
-
-				if (props['iso:country']) {
-					self.set_property('iso:country', props['iso:country']);
-				}
-				if (props['wof:country']) {
-					self.set_property('wof:country', props['wof:country']);
-				}
-
-				if (cb) {
-					cb(rsp);
-				}
-			};
-
-			var on_failure = function(rsp) {
-				mapzen.whosonfirst.log.error('Failed to set country properties.');
+			if (props['iso:country']) {
+				self.set_property('iso:country', props['iso:country']);
 			}
-
-			mapzen.whosonfirst.net.fetch(url, on_success, on_failure);
-		},
-
-		set_property: function(name, value) {
-			self.properties[name] = value;
+			if (props['wof:country']) {
+				self.set_property('wof:country', props['wof:country']);
+			}
 		},
 
 		generate_feature: function() {
@@ -119,12 +99,95 @@ mapzen.whosonfirst.boundaryissues.venue = (function() {
 
 			mapzen.whosonfirst.boundaryissues.api.api_call("wof.save", data, onsuccess, onerror);
 		},
+
+		'lookup_hierarchy': function(ll) {
+
+			var data = {
+				latitude: ll.lat,
+				longitude: ll.lng,
+				placetype: 'venue'
+			};
+
+			var onsuccess = function(rsp) {
+				if (! rsp.ok) {
+					mapzen.whosonfirst.log.error('Error reverse geocoding.');
+					return;
+				}
+				if (rsp.parents && rsp.parents.length == 1) {
+					self.set_property('wof:parent_id', rsp.parents[0].Id);
+				} else {
+					self.set_property('wof:parent_id', -1);
+				}
+				if (rsp.hierarchy) {
+					self.set_property('wof:hierarchy', rsp.hierarchy);
+				} else {
+					self.set_property('wof:hierarchy', []);
+				}
+			}
+
+			var onerror = function(rsp) {
+				mapzen.whosonfirst.log.error('Error reverse geocoding.');
+			};
+
+			mapzen.whosonfirst.boundaryissues.api.api_call("wof.pip", data, onsuccess, onerror);
+		},
+
+		'show_feature_pin': function(map, geocoder, feature) {
+			var html = '<a href="#" class="btn btn-primary" id="geocoder-marker-select">Use this result</a> <a href="#" class="btn" id="geocoder-marker-cancel">Cancel</a>';
+			var popup = geocoder.marker.bindPopup(html).openPopup();
+			var props = feature.properties;
+			if (feature.bbox) {
+				mapzen.whosonfirst.boundaryissues.bbox.set_bbox(map, feature.bbox);
+			}
+			$('#geocoder-marker-select').click(function(e) {
+				e.preventDefault();
+				popup.closePopup();
+				geocoder.collapse();
+				var ll = geocoder.marker.getLatLng();
+				map.removeLayer(geocoder.marker);
+				map.setView(ll, 16);
+				//self.lookup_hierarchy(ll.lat, ll.lng);
+				//self.update_coordinates(ll, true);
+				//self.set_marker(geocoder.marker);
+
+				// This is a US-centric way of encoding an address
+				var regex = new RegExp('^' + props.name);
+				var address = props.label.replace(regex, props.housenumber + ' ' + props.street);
+
+				if (! $('textarea[name="address"]').val()) {
+					$('textarea[name="address"]').val(address);
+				}
+				self.set_property('addr:full', address);
+
+				if (props.housenumber) {
+					self.set_property('addr:housenumber', props.housenumber);
+				}
+				if (props.street) {
+					self.set_property('addr:street', props.street);
+				}
+				if (props.postalcode) { // Seeing postAL code coming from Pelias, but we prefer 'postcode'
+					self.set_property('addr:postcode', props.postalcode);
+				}
+				if (props.postcode) {
+					self.set_property('addr:postcode', props.postcode);
+				}
+			});
+			$('#geocoder-marker-cancel').click(function(e) {
+				e.preventDefault();
+				popup.closePopup();
+				map.removeLayer(geocoder.marker);
+			});
+		}
 	};
 
 	function setup_map() {
 
 		var scene = mapzen.whosonfirst.boundaryissues.utils.abs_root_urlify('/tangram/refill.yaml');
 		mapzen.whosonfirst.leaflet.tangram.scenefile(scene);
+
+		var map = mapzen.whosonfirst.leaflet.tangram.map('map');
+		self.map = map;
+		var hash = new L.Hash(map);
 
 		VenueIcon = L.Icon.extend({
 			options: {
@@ -137,128 +200,28 @@ mapzen.whosonfirst.boundaryissues.venue = (function() {
 			}
 		});
 
-		var show_the_map = function(bbox) {
-			var map = mapzen.whosonfirst.leaflet.tangram.map_with_bbox(
-				'map',
-				bbox[1],
-				bbox[0],
-				bbox[3],
-				bbox[2]
-			);
-
-			var geocoder = L.control.geocoder('search-o3YYmTI', {
-				markers: {
-					icon: new VenueIcon()
-				}
-			}).addTo(map);
-
-			var hash = new L.Hash(map);
-
-			function lookup_hierarchy() {
-				var ll = map.getCenter();
-				var data = {
-					latitude: ll.lat,
-					longitude: ll.lng,
-					placetype: 'venue'
-				};
-
-				var onsuccess = function(rsp) {
-					if (! rsp.ok) {
-						mapzen.whosonfirst.log.error('Error reverse geocoding.');
-						return;
-					}
-					if (rsp.parents && rsp.parents.length == 1) {
-						self.set_property('wof:parent_id', rsp.parents[0].Id);
-					} else {
-						self.set_property('wof:parent_id', -1);
-					}
-					if (rsp.hierarchy) {
-						self.set_property('wof:hierarchy', rsp.hierarchy);
-					} else {
-						self.set_property('wof:hierarchy', []);
-					}
-				}
-
-				var onerror = function(rsp) {
-					mapzen.whosonfirst.log.error('Error reverse geocoding.');
-				};
-
-				mapzen.whosonfirst.boundaryissues.api.api_call("wof.pip", data, onsuccess, onerror);
+		var geocoder = L.control.geocoder('search-o3YYmTI', {
+			markers: {
+				icon: new VenueIcon()
 			}
+		}).addTo(map);
 
-			map.on('moveend', lookup_hierarchy);
+		mapzen.whosonfirst.boundaryissues.bbox.init(map, function(rsp) {
+			self.set_country(rsp.country);
+			slippymap.crosshairs.init(map);
+			if ($(document.body).hasClass('user-signed-in')) {
+				mapzen.whosonfirst.boundaryissues.bbox.draw_set_default_link(map);
+			}
+		});
 
-			geocoder.on('select', function(e) {
-				var html = '<a href="#" class="btn btn-primary" id="geocoder-marker-select">Use this result</a> <a href="#" class="btn" id="geocoder-marker-cancel">Cancel</a>';
-				var popup = geocoder.marker.bindPopup(html).openPopup();
-				var props = e.feature.properties;
-				if (e.feature.bbox) {
-					var bbox = e.feature.bbox;
-					var sw = L.latLng(bbox[1], bbox[0]);
-					var ne = L.latLng(bbox[3], bbox[2]);
-					var bounds = L.latLngBounds(sw, ne);
-					map.fitBounds(bounds);
-				}
-				$('#geocoder-marker-select').click(function(e) {
-					e.preventDefault();
-					popup.closePopup();
-					geocoder.collapse();
-					var ll = geocoder.marker.getLatLng();
-					map.removeLayer(geocoder.marker);
-					map.setView(ll, 16);
-					//self.lookup_hierarchy(ll.lat, ll.lng);
-					//self.update_coordinates(ll, true);
-					//self.set_marker(geocoder.marker);
+		map.on('moveend', function() {
+			var ll = map.getCenter();
+			self.lookup_hierarchy(ll);
+		});
 
-					// This is a US-centric way of encoding an address
-					var regex = new RegExp('^' + props.name);
-					var address = props.label.replace(regex, props.housenumber + ' ' + props.street);
-
-					if (! $('input[name="name"]').val()) {
-						$('input[name="name"]').val(props.name);
-					}
-					if (! $('textarea[name="address"]').val()) {
-						$('textarea[name="address"]').val(address);
-					}
-
-					self.set_property('addr:full', address);
-
-					if (props.housenumber) {
-						self.set_property('addr:housenumber', props.housenumber);
-					}
-					if (props.street) {
-						self.set_property('addr:street', props.street);
-					}
-					if (props.postalcode) { // Seeing postAL code coming from Pelias, but we prefer 'postcode'
-						self.set_property('addr:postcode', props.postalcode);
-					}
-					if (props.postcode) {
-						self.set_property('addr:postcode', props.postcode);
-					}
-				});
-				$('#geocoder-marker-cancel').click(function(e) {
-					e.preventDefault();
-					popup.closePopup();
-					map.removeLayer(geocoder.marker);
-				});
-			});
-			return map;
-		};
-
-		var onerror = function(rsp) {
-			mapzen.whosonfirst.log.error("could not load ip service");
-		};
-
-		var onsuccess = function(rsp) {
-			self.set_country(rsp.country_id, function(country) {
-				var map = show_the_map(country.bbox);
-				self.map = map;
-				slippymap.crosshairs.init(map);
-			});
-		};
-
-		var url = 'https://ip.dev.mapzen.com/?raw=1';
-		mapzen.whosonfirst.net.fetch(url, onsuccess, onerror);
+		geocoder.on('select', function(e) {
+			self.show_feature_pin(map, geocoder, e.feature);
+		});
 	}
 
 	function setup_form() {
