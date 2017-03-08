c2f4bc5d390b0a7a88f29a420941a7e02d873d25
Dan Phiffer <dan@phiffer.org>
2017-03-08 16:35:02 -0500

    :bug: be more careful with opening the dbug log

--- www/include/lib_dbug.php
+++ www/include/lib_dbug.php
@@ -7,7 +7,12 @@ function dbug() {
 
 	if (! $GLOBALS['dbug_fh']) {
 		$root = dirname(dirname(__DIR__));
-		$GLOBALS['dbug_fh'] = fopen($GLOBALS['cfg']['dbug_log'], 'a');
+		$GLOBALS['dbug_fh'] = @fopen($GLOBALS['cfg']['dbug_log'], 'a');
+	}
+
+	if (! is_resource($GLOBALS['dbug_fh'])) {
+		error_log("Could not write dbug() message to {$GLOBALS['cfg']['dbug_log']}");
+		return;
 	}
 
 	$args = func_get_args();
