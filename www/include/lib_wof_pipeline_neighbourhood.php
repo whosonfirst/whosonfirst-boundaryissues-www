f72c9329f5928526ca4b8eaad0a1762469878f28
Dan Phiffer <dan@phiffer.org>
2017-06-01 12:28:20 -0400

    :speech_balloon: be less chatty in the logs; simulate coin flip failures/success

--- www/include/lib_wof_pipeline_neighbourhood.php
+++ www/include/lib_wof_pipeline_neighbourhood.php
@@ -1,6 +1,15 @@
 <?php
 
 	function wof_pipeline_neighbourhood($pipeline, $dry_run = false) {
-		wof_pipeline_log($pipeline['id'], "This is a mock pipeline; bailing out");
-		return array('ok' => 0);
+		wof_pipeline_log($pipeline['id'], "Running wof_pipeline_neighbourhood", array(
+			'dry_run' => $dry_run
+		));
+		$coin_toss = rand(0, 1); // Sometimes yes sometimes no
+		if ($coin_toss) {
+			wof_pipeline_log($pipeline['id'], "This is a mock pipeline; simulating failure");
+			return array('ok' => 0);
+		} else {
+			wof_pipeline_log($pipeline['id'], "This is a mock pipeline; simulating success");
+			return array('ok' => 1);
+		}
 	}
