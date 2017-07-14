07b54f35cbf2ddf6081f5012bff7b0bc7cd81466
Dan Phiffer <dan@phiffer.org>
2017-07-14 14:22:00 +0900

    :sparkles: get/set repo status scripts

---
+++ bin/get_repo_status.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'init_local.php';
+loadlib('wof_repo');
+
+if (count($argv) < 2) {
+	die("Usage: php get_repo_status.php [repo]\n");
+}
+
+$repo = $argv[1];
+$rsp = wof_repo_get_status($repo);
+if ($rsp['ok'] && $rsp['status']) {
+	echo "$repo: {$rsp['status']}\n";
+} else {
+	var_export($rsp);
+}
---
+++ bin/set_repo_status.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'init_local.php';
+loadlib('wof_repo');
+
+if (count($argv) < 3) {
+	die("Usage: php set_repo_status.php [repo] [status]\n");
+}
+
+$repo = $argv[1];
+$status = $argv[2];
+$rsp = wof_repo_set_status($repo, $status);
+if ($rsp['ok']) {
+	echo "$repo: $status\n";
+} else {
+	var_export($rsp);
+}
