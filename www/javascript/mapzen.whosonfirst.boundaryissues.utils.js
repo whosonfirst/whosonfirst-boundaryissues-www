b6e1224efc244cc406ea73e8ab1b973295271bdc
Dan Phiffer <dan@phiffer.org>
2017-07-26 12:21:16 +0800

    :sparkles: mapzen.whosonfirst.utils.get_meta_file(filename, cb)

--- www/javascript/mapzen.whosonfirst.boundaryissues.utils.js
+++ www/javascript/mapzen.whosonfirst.boundaryissues.utils.js
@@ -23,7 +23,19 @@ mapzen.whosonfirst.boundaryissues.utils = (function(){
 			}
 
 			return root + url;
-		}
+		},
+
+		'get_meta_file': function(filename, cb){
+			var url = mapzen.whosonfirst.boundaryissues.utils.abs_root_urlify('/meta/' + filename);
+			var onsuccess = function(result){
+				cb(result);
+			};
+			var onerror = function(){
+				mapzen.whosonfirst.log.debug("error loading " + filename + ".");
+			};
+			var cache_ttl = 60 * 60 * 1000; // one hour
+			return mapzen.whosonfirst.net.fetch(url, onsuccess, onerror, cache_ttl);
+		},
 	};
 
 	return self
