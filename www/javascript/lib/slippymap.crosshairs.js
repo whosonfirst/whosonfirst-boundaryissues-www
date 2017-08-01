f988e98dce557f06a0c2da29481ab7e3275bcf74
Dan Phiffer <dan@phiffer.org>
2017-08-01 05:26:54 -0700

    :wrench: fix whitespace

--- www/javascript/lib/slippymap.crosshairs.js
+++ www/javascript/lib/slippymap.crosshairs.js
@@ -2,133 +2,133 @@ var slippymap = slippymap || {};
 
 slippymap.crosshairs = (function(){
 
-    var latlon = true;
+	var latlon = true;
 
-    var self = {
+	var self = {
 
-	'init': function(map){
+		'init': function(map){
 
-	    var container = map.getContainer();
-	    var id = container.id;
+			var container = map.getContainer();
+			var id = container.id;
 
-	    var draw = function(){
-		self.draw_crosshairs(id);
-	    };
+			var draw = function(){
+				self.draw_crosshairs(id);
+			};
 
-	    window.onresize = draw;
+			window.onresize = draw;
 
-	    var coords = function(){
-		self.draw_coords(map);
-	    };
+			var coords = function(){
+				self.draw_coords(map);
+			};
 
-	    map.on('move', coords);
-	    map.on('dragend', coords);
-	    map.on('zoomend', coords);
-	
-	    // because for SOME REASON these don't both work reliably in map.on('load')
-	    // because... COMPUTERS? (20160809/thisisaaronland)
+			map.on('move', coords);
+			map.on('dragend', coords);
+			map.on('zoomend', coords);
 
-	    draw();
-	    coords();
-	},
+			// because for SOME REASON these don't both work reliably in map.on('load')
+			// because... COMPUTERS? (20160809/thisisaaronland)
 
-	'draw_coords': function(map){
+			draw();
+			coords();
+		},
 
-	    var coords = document.getElementById("slippymap-coords");
+		'draw_coords': function(map){
 
-	    if (! coords){
+			var coords = document.getElementById("slippymap-coords");
 
-		var coords = document.createElement("div");
-		coords.setAttribute("id", "slippymap-coords");
+			if (! coords){
 
-		coords.onclick = function(){
-		    latlon = (latlon) ? false : true;
-		    self.draw_coords(map);
-		    return;
-		};
+				var coords = document.createElement("div");
+				coords.setAttribute("id", "slippymap-coords");
 
-		var container = map.getContainer();
-		var container_el = document.getElementById(container.id);
+				coords.onclick = function(){
+					latlon = (latlon) ? false : true;
+					self.draw_coords(map);
+					return;
+				};
 
-		container_el.parentNode.insertBefore(coords, container_el.nextSibling); 
-	    }
+				var container = map.getContainer();
+				var container_el = document.getElementById(container.id);
 
-	    var pos = map.getCenter();
-	    var lat = pos['lat'];
-	    var lon = pos['lng'];	  
-	    
-	    var zoom = map.getZoom();
+				container_el.parentNode.insertBefore(coords, container_el.nextSibling);
+			}
 
-	    var ll = undefined;
-	    var title = undefined;
+			var pos = map.getCenter();
+			var lat = pos['lat'];
+			var lon = pos['lng'];
 
-	    if (latlon){
+			var zoom = map.getZoom();
 
-		    ll = lat.toFixed(6) + ", " + lon.toFixed(6) + " #" + zoom.toFixed(2);
-		title = "coordinates are displayed as latitude,longitude – click to toggle";
-	    }
-	    
-	    else {
+			var ll = undefined;
+			var title = undefined;
 
-		ll = lon.toFixed(6) + ", " + lat.toFixed(6) + " #" + zoom;
-		title = "coordinates are displayed as longitude,latitude – click to toggle";
-	    }
+			if (latlon){
 
-	    coords.setAttribute("title", title);
-	    coords.innerText = ll;	    
-	},
-	
-	'draw_crosshairs': function(id){
+				ll = lat.toFixed(6) + ", " + lon.toFixed(6) + " #" + zoom.toFixed(2);
+				title = "coordinates are displayed as latitude,longitude – click to toggle";
+			}
 
-	    var m = document.getElementById(id);
+			else {
 
-	    if (! m){
-		return false;
-	    }
+				ll = lon.toFixed(6) + ", " + lat.toFixed(6) + " #" + zoom;
+				title = "coordinates are displayed as longitude,latitude – click to toggle";
+			}
 
-	    var container = m.getBoundingClientRect();
-	    
-	    var height = container.height;
-	    var width = container.width;
-	    
-	    var crosshair_y = (height / 2) - 8;
-	    var crosshair_x = (width / 2);
-	    
-	    // http://www.sveinbjorn.org/dataurls_css
-	    
-	    var data_url = '"data:image/gif;base64,R0lGODlhEwATAKEBAAAAAP///////////' + 
-		'yH5BAEKAAIALAAAAAATABMAAAIwlI+pGhALRXuRuWopPnOj7hngEpRm6Z' + 
-		'ymAbTuC7eiitJlNHr5tmN99cNdQpIhsVIAADs="';
-	    
-	    var style = [];
-	    style.push("position:absolute");
-	    style.push("top:" + crosshair_y + "px");
-	    style.push("height:19px");
-	    style.push("width:19px");
-	    style.push("left:" + crosshair_x + "px");
-	    style.push("margin-left:-8px;");
-	    style.push("display:block");
-	    style.push("background-position: center center");
-	    style.push("background-repeat: no-repeat");
-	    style.push("background: url(" + data_url + ")");
-	    style.push("z-index:10000");
-	    
-	    style = style.join(";");
+			coords.setAttribute("title", title);
+			coords.innerText = ll;
+		},
 
-	    var crosshairs = document.getElementById("slippymap-crosshairs");
+		'draw_crosshairs': function(id){
 
-	    if (! crosshairs){
+			var m = document.getElementById(id);
 
-		crosshairs = document.createElement("div");
-		crosshairs.setAttribute("id", "slippymap-crosshairs");
-		m.appendChild(crosshairs);
-	    }
+			if (! m){
+				return false;
+			}
 
-	    crosshairs.style.cssText = style;
-	    return true;
-	},
-    };
+			var container = m.getBoundingClientRect();
 
-    return self;
+			var height = container.height;
+			var width = container.width;
+
+			var crosshair_y = (height / 2) - 8;
+			var crosshair_x = (width / 2);
+
+			// http://www.sveinbjorn.org/dataurls_css
+
+			var data_url = '"data:image/gif;base64,R0lGODlhEwATAKEBAAAAAP///////////' +
+			'yH5BAEKAAIALAAAAAATABMAAAIwlI+pGhALRXuRuWopPnOj7hngEpRm6Z' +
+			'ymAbTuC7eiitJlNHr5tmN99cNdQpIhsVIAADs="';
+
+			var style = [];
+			style.push("position:absolute");
+			style.push("top:" + crosshair_y + "px");
+			style.push("height:19px");
+			style.push("width:19px");
+			style.push("left:" + crosshair_x + "px");
+			style.push("margin-left:-8px;");
+			style.push("display:block");
+			style.push("background-position: center center");
+			style.push("background-repeat: no-repeat");
+			style.push("background: url(" + data_url + ")");
+			style.push("z-index:10000");
+
+			style = style.join(";");
+
+			var crosshairs = document.getElementById("slippymap-crosshairs");
+
+			if (! crosshairs){
+
+				crosshairs = document.createElement("div");
+				crosshairs.setAttribute("id", "slippymap-crosshairs");
+				m.appendChild(crosshairs);
+			}
+
+			crosshairs.style.cssText = style;
+			return true;
+		},
+	};
+
+	return self;
 
 })();
