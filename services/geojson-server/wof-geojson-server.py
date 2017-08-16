b0ec799a3152d60af400d02dcd37f47867e6b3ab
Dan Phiffer <dan@phiffer.org>
2017-08-16 10:56:42 -0700

    :white_check_mark: update validation for new geojson library version

--- services/geojson-server/wof-geojson-server.py
+++ services/geojson-server/wof-geojson-server.py
@@ -37,11 +37,8 @@ def geojson_encode():
 		logging.error(error)
 		return jsonify(ok=0, error=error)
 
-	# Does the input pass the smell check?
-	validation = geojson.is_valid(f)
-
-	if (validation['valid'] == 'no'):
-		error = "GeoJSON doesn't smell right: %s" % validation['message']
+	if (f.is_valid == False):
+		error = "GeoJSON doesn't validate: %s" % f.errors()
 		logging.error(error)
 		return jsonify(ok=0, error=error)
 
