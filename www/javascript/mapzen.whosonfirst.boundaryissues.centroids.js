10f9b2b41dcacbd9390a3e5ad083894b86cc0a12
Dan Phiffer <dan@phiffer.org>
2017-11-09 16:33:03 -0500

    :pencil2: proper spec structure / add new centroid
    re: #225

--- www/javascript/mapzen.whosonfirst.boundaryissues.centroids.js
+++ www/javascript/mapzen.whosonfirst.boundaryissues.centroids.js
@@ -8,16 +8,38 @@ mapzen.whosonfirst.boundaryissues = mapzen.whosonfirst.boundaryissues || {};
 
 mapzen.whosonfirst.boundaryissues.centroids = (function() {
 
-	var centroids = {
+	var spec = {
 		'geom': {
 			name: 'math centroid',
+			type: 'common',
+			description: 'The math centroid of the area polygon.',
 			readonly: true
 		},
+		'intersection': {
+			type: 'optional'
+		},
 		'lbl': {
-			name: 'label centroid'
+			name: 'label centroid',
+			type: 'common',
+			description: 'Where the label should be displayed.'
+		},
+		'local': {
+			type: 'optional',
+			description: 'Where do locals from this place consider the "center".'
+		},
+		'nav': {
+			name: 'navigation centroid',
+			type: 'common-optional',
+			description: 'Snapped to nearest road and used to provide accurate turn-by-turn directions and navigation ETAs (expected time of arrivals).'
 		},
 		'reversegeo': {
-			name: 'PIP centroid'
+			name: 'PIP centroid',
+			type: 'common-optional',
+			description: 'Used for point-in-polygon (PIP) processing.'
+		},
+		'tourist': {
+			type: 'optional',
+			description: 'Tourists follow guides and flock towards famous attractions.'
 		}
 	};
 
@@ -27,8 +49,8 @@ mapzen.whosonfirst.boundaryissues.centroids = (function() {
 			var lat = centroid.lat;
 			var lng = centroid.lng;
 			var name = centroid.prefix + ' centroid';
-			if (centroids[centroid.prefix]) {
-				name = centroids[centroid.prefix].name;
+			if (spec[centroid.prefix]) {
+				name = spec[centroid.prefix].name;
 			}
 			var html = name + ': <strong>' + lat + ', ' + lng + '</strong>' +
 			           '<span id="where-parent"></span>';
@@ -45,7 +67,31 @@ mapzen.whosonfirst.boundaryissues.centroids = (function() {
 		},
 
 		editor_init: function() {
+			var options = '';
+			for (var prefix in spec) {
+				options += '<option>' + prefix + '</option>';
+			}
+			var html = '<div id="centroids-editor">' +
+			           '<select id="centroids-select">' + options + '</select> ' +
+			           '<strong id="centroids-coords"></strong>' +
+			           '</div>';
 
+			$('#where').html(html);
+			$('#centroids-select').change(self.update_coords);
+			self.update_coords();
+		},
+
+		update_coords: function() {
+			var prefix = $('#centroids-select').val();
+			var lat = $('input[name="properties.' + prefix + ':latitude"]').val();
+			var lng = $('input[name="properties.' + prefix + ':longitude"]').val();
+			if (lat && lng) {
+				lat = parseFloat(lat).toFixed(6);
+				lng = parseFloat(lng).toFixed(6);
+				$('#centroids-coords').html(lat + ', ' + lng);
+			} else {
+				$('#centroids-coords').html('<a href="#">add new centroid</a>');
+			}
 		}
 
 	};
