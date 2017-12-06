45313c8445c3c58e509bd5d7a9b5f62db2b34214
Dan Phiffer <dan@phiffer.org>
2017-12-06 15:31:41 -0500

    :dizzy: lib_bcrypt

--- www/include/lib_bcrypt.php
+++ www/include/lib_bcrypt.php
@@ -3,15 +3,19 @@
 # lib_bcrypt - bcrypt hashing in PHP
 #
 # Based on Portable PHP password hashing framework:
-# 
+#
 #	http://www.openwall.com/phpass/
 #
 # There's absolutely no warranty.
 #
+
+# PHP Deprecated:  Methods with the same name as their class will not be constructors in a future version of PHP; BCryptHasher has a deprecated constructor in /home/ubuntu/flamework/www/include/lib_bcrypt.php on line 11
+
 class BCryptHasher {
 	private $random_state;
 
-	function BCryptHasher()
+	//function BCryptHasher()
+	function __construct()
 	{
 		if (CRYPT_BLOWFISH != 1) die("lib_bcyrpt requires CRYPT_BLOWFISH PHP support!");
 
@@ -101,4 +105,3 @@ class BCryptHasher {
 		return $hash == $stored_hash;
 	}
 }
-
